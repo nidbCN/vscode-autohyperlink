b23b3720397901ad74c32e894ecd1b1be2c1e3e1
Gaein nidb <mail@gaein.cn>
2022-08-28 16:01:36 +0800

    ✨ feat(插入链接): http请求和标题获取

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const request = require('request');
+const cheerio = require('cheerio');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,11 +11,6 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autohyperlink" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -21,6 +18,18 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		vscode.window.showInputBox({ title: 'url', value: 'https://' })
+			.then(value => {
+				request.get(value, (error, response, data) => {
+					if (error) {
+						vscode.window.showErrorMessage(error);
+					} else {
+						const html = cheerio.load(data);
+						const title = html('title').html();
+						vscode.window.showInformationMessage(title);
+					}
+				})
+				vscode.window.showInformationMessage(value);
+			});
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from AutoHyperlink!');
 	});
